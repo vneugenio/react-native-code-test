14629596d58da51db353ca9f4c9e796a5f5d54bf
bolts <voltaire.eugenio@gmail.com>
2018-10-01 03:18:37 +0800

    initial commit

--- app/index.js
+++ app/index.js
@@ -9,50 +9,101 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableOpacity,
+  Dimensions,
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+class FirstChild extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-type Props = {};
-export default class App extends Component<Props> {
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={styles.containerChild}>
+        <Image
+          source={require('./components/img/react-logo.png')}
+          style={styles.mainLogo}
+        />
+        <TouchableOpacity
+          onPress={() => {}}
+          style={styles.fetchButton}
+        >
+          <Text style={styles.buttonText}>Fetch Data</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.containerMain}>
+        <FirstChild />
+      </View>
+    );
+  }
+}
+
+const viewportHeight = Dimensions.get('window').height;
+const viewportWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
-  container: {
+  containerMain: {
+    backgroundColor: '#000000',
+    width: viewportWidth,
+    height: viewportHeight,
+    position: 'absolute',
+    zIndex: 1,
+    top: 0,
+    left: 0,
+  },
+  containerChild: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#ffffff',
+    position: 'absolute',
+    width: viewportWidth,
+    height: viewportHeight,
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    color: '#ffffff',
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#ffffff',
     marginBottom: 5,
+  },
+  mainLogo: {
+    width: 250,
+    height: 250,
+  },
+  fetchButton: {
+    borderRadius: 5,
+    borderColor: '#02D5DB',
+    borderWidth: 8,
+    borderStyle: 'solid',
+    height: 60,
+    width: 300,
+    backgroundColor: '#32dfff',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 25,
+    fontWeight: '600',
   },
 });
